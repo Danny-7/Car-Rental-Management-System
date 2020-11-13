3bf6554e2f0fbeae7fd09c914f02ab1d950d5e86
Hugo Mk <hugomiko78@hotmail.fr>
2020-11-13 23:03:09 +0100

    lastfix

--- db.sql
+++ db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 11, 2020 at 10:52 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.11
+-- Host: localhost:3306
+-- Generation Time: Nov 13, 2020 at 09:53 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -22,56 +23,33 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
 --
--- Table structure for table `billing`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `billing` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `id_user_id` int(11) NOT NULL,
-  `id_car_id` int(11) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date DEFAULT NULL,
-  `price` double NOT NULL,
-  `paid` tinyint(1) NOT NULL,
-  `returned` tinyint(1) NOT NULL
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `billing`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `billing` (`id`, `id_user_id`, `id_car_id`, `start_date`, `end_date`, `price`, `paid`, `returned`) VALUES
-(47, 7, 8, '2020-11-05', NULL, 125, 0, 1),
-(48, 7, 7, '2020-11-05', '2020-11-14', 250, 1, 1),
-(49, 7, 4, '2020-11-04', '2020-11-06', 400, 1, 1),
-(50, 7, 3, '2020-11-05', NULL, 300, 0, 0),
-(51, 7, 8, '2020-11-05', '2020-11-07', 125, 1, 1),
-(52, 7, 8, '2020-11-06', '2020-11-07', 125, 1, 1),
-(53, 6, 4, '2020-11-08', '2020-11-11', 400, 1, 1),
-(54, 6, 4, '2020-11-08', '2020-11-11', 400, 1, 1),
-(55, 6, 7, '2020-11-14', NULL, 250, 0, 0),
-(56, 6, 8, '2020-11-13', '2020-11-20', 125, 1, 1),
-(57, 6, 8, '2020-11-13', '2020-11-20', 125, 1, 1),
-(58, 6, 5, '2020-11-11', '2020-11-13', 600, 1, 1),
-(59, 6, 7, '2020-11-08', '2020-11-09', 250, 1, 1),
-(60, 6, 7, '2020-11-08', '2020-11-09', 250, 1, 1),
-(61, 6, 7, '2020-11-14', '2020-11-21', 250, 1, 1),
-(62, 6, 1, '2020-11-08', '2020-11-10', 300, 1, 1),
-(63, 6, 1, '2020-11-08', '2020-11-10', 300, 1, 1),
-(64, 6, 1, '2020-11-08', '2020-11-10', 300, 1, 1),
-(65, 6, 3, '2020-11-08', '2020-11-09', 270, 1, 1),
-(66, 7, 1, '2020-11-10', '2020-11-11', 300, 1, 1),
-(67, 7, 1, '2020-11-10', '2020-11-11', 300, 1, 1),
-(68, 7, 4, '2020-11-10', '2020-11-12', 400, 1, 1),
-(69, 6, 2, '2020-11-26', NULL, 3240, 0, 0),
-(70, 6, 2, '2020-11-26', NULL, 3240, 0, 0),
-(71, 6, 3, '2020-11-27', NULL, 8100, 0, 0),
-(72, 6, 3, '2020-11-27', NULL, 8100, 0, 0);
+INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'Admin', 'admin@rentcar.com', '$2y$13$YYseT2BsUH9745hHorvwTeufg/P6UOT46V/uwRFKDEnMtuavC7uRO', 'ROLE_ADMIN'),
+(2, 'Hubert Pichet', 'hubert.pichet@gmail.com', '$2y$13$yy4V7E2cjmZZXpSLilfv7uMHHs6a6v43bIuZSwy2UshF4OdjFauv6', 'ROLE_CLIENT'),
+(3, 'Easy Rent', 'easyrent@easyrent.com', '$2y$13$Rsd2.JuG3IOiVXxrtM1a3OA2rMHhOS7SBsDaGZGML8ishREbep.nW', 'ROLE_RENTER'),
+(4, 'Sixt', 'sixt@sixt.com', '$2y$13$hYDwhipyPK4I8iffsSQLzu9VSYL.oZvqsaaOJzjt/xaPL93f8h6sy', 'ROLE_RENTER'),
+(5, 'Hertz', 'hertz@hertz.com', '$2y$13$eo5X7iqP7H/HkRrANGXLZOszb03coeo8il6ffo9yD/9P/FsRiJo9y', 'ROLE_RENTER'),
+(6, 'Jerome Aurore', 'jerome.aurore@hotmail.com', '$2y$13$i0kTXAFNDHxAdOiknKVnmuGX.FcjwGHo9ajP.JMg9wKwd9.2wgJ0i', 'ROLE_CLIENT'),
+(7, 'Lena Selim', 'selimlena@gmail.com', '$2y$13$fREKb5oUSUZ11w8Guj8AwuVJbbKbglLfIp5yPOnOqiHYTI4rlwfjS', 'ROLE_CLIENT');
+
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `car`
 --
@@ -92,17 +70,51 @@ CREATE TABLE `car` (
 --
 
 INSERT INTO `car` (`id`, `id_owner_id`, `type`, `datasheet`, `amount`, `rent`, `image`, `quantity`) VALUES
-(1, 1, 'Mercedes Classe A', '{\"category\":\"Citadine\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 300, 'disponible', 'mercedesA.png', 5),
-(2, 1, 'Clio 4', '{\"category\":\"Citadine\",\"fuel\":\"Diesel\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 120, 'disponible', 'S7-modele-renault-clio-4.jpeg', 1),
-(3, 1, 'Audi A3 berline', '{\"category\":\"Berline\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 300, 'disponible', 'audi-a3.jpeg', 9),
-(4, 3, 'Mercedes Classe C', '{\"category\":\"Berline\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 400, 'disponible', 'mercedes-benz-c-class.jpeg', 6),
-(5, 3, 'Porsche Macan  Turbo S 2020', '{\"category\":\"4x4, SUV, Crossover\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 600, 'disponible', 'macan.png', 9),
-(6, 3, 'Porsche Cayenne 2018', '{\"category\":\"4x4, SUV, Crossover\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 700, 'disponible', 'cayennejpg.jpeg', 2),
-(7, 3, 'Volkswagen Golf 7', '{\"category\":\"Citadine\",\"fuel\":\"Diesel\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 250, 'disponible', 'golf.jpeg', 10),
-(8, 3, 'Peugeot 208 2018', '{\"category\":\"Citadine\",\"fuel\":\"Diesel\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 125, 'disponible', 'peugeot208.jpeg', 20);
+(1, 4, 'Peugeot 2008', '{\"category\":\"4x4, SUV, Crossover\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 120, 'disponible', '2008.jpeg', 6),
+(2, 4, 'Renault Clio 5', '{\"category\":\"Citadine\",\"fuel\":\"Diesel\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 100, 'disponible', 'clio5.jpeg', 10),
+(3, 4, 'Volkswagen Golf 7', '{\"category\":\"Berline\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 110, 'disponible', 'volkswagen-golf-7.jpeg', 4),
+(4, 3, 'Mercedes CLA', '{\"category\":\"Break\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Manuelle\",\"nb_portes\":\"5 portes\"}', 220, 'disponible', 'mercedes-cla.jpeg', 2),
+(5, 3, 'Abarth 500', '{\"category\":\"Cabriolet\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"3 portes\"}', 140, 'disponible', 'abarth.jpeg', 2),
+(6, 5, 'Audi RS6', '{\"category\":\"Break\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 230, 'disponible', 'audi-rs6.jpeg', 1),
+(7, 5, 'BMW X5', '{\"category\":\"4x4, SUV, Crossover\",\"fuel\":\"Diesel\",\"engine\":\"Hybride\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 200, 'disponible', 'bmw-x5.jpeg', 4),
+(8, 5, 'Lamborghini Urus', '{\"category\":\"4x4, SUV, Crossover\",\"fuel\":\"Essence\",\"engine\":\"Thermique\",\"shift\":\"Automatique\",\"nb_portes\":\"5 portes\"}', 350, 'disponible', 'urus.jpeg', 2);
+
 
 -- --------------------------------------------------------
+--
+-- Table structure for table `billing`
+--
 
+CREATE TABLE `billing` (
+  `id` int(11) NOT NULL,
+  `id_user_id` int(11) NOT NULL,
+  `id_car_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `price` double NOT NULL,
+  `paid` tinyint(1) NOT NULL,
+  `returned` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `billing`
+--
+
+INSERT INTO `billing` (`id`, `id_user_id`, `id_car_id`, `start_date`, `end_date`, `price`, `paid`, `returned`) VALUES
+(1, 2, 1, '2020-11-13', '2020-11-14', 120, 1, 1),
+(2, 2, 2, '2020-11-14', '2020-11-18', 400, 1, 1),
+(3, 2, 3, '2020-11-14', NULL, 3300, 0, 0),
+(4, 2, 2, '2020-11-14', '2020-11-20', 600, 1, 0),
+(5, 6, 2, '2020-11-13', '2020-11-14', 100, 1, 1),
+(6, 6, 5, '2020-11-14', NULL, 4200, 0, 0),
+(7, 6, 6, '2020-11-14', NULL, 6900, 0, 0),
+(8, 7, 7, '2020-11-14', '2020-11-19', 1000, 1, 1),
+(9, 7, 1, '2020-11-14', '2020-12-04', 2400, 0, 0),
+(10, 7, 4, '2020-11-14', '2020-11-28', 3080, 1, 0),
+(11, 7, 4, '2020-11-13', '2020-11-15', 440, 1, 1);
+
+
+-- --------------------------------------------------------
 --
 -- Table structure for table `comment`
 --
@@ -120,38 +132,13 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `car_id`, `author`, `content`, `created_at`) VALUES
-(3, 3, 'Hugo', 'Je suis pleinement satisfait de ma location. Voiture spacieuse et confortable. Je recommande !', '2020-11-08 04:06:45'),
-(4, 8, 'Hubert', 'Super voiture ! Je l\'ai eu pendant 2 jours et c\'était parfait : confortable et spacieuse.', '2020-11-08 04:40:22'),
-(5, 4, 'Hubert', 'Superbe voiture !', '2020-11-08 23:48:06');
+(1, 1, 'Hubert Pichet', 'Je suis pleinement satisfait de ce véhicule. Il est très spacieux !', '2020-11-13 21:31:25'),
+(2, 2, 'Hubert Pichet', 'Très content de ma location chez RentCar ! Superbe voiture !', '2020-11-13 21:32:21'),
+(3, 2, 'Jerome Aurore', 'Je recommande cette voiture ! J\'ai passé un agréable moment à la conduire : très confortable et assez spacieuse.', '2020-11-13 21:36:54'),
+(4, 7, 'Lena Selim', 'Cette voiture est géniale ! Le X5 m\'a beaucoup plu et je le recommande !', '2020-11-13 21:41:35'),
+(5, 4, 'Lena Selim', 'Encore satisfaite de RentCar, merci pour ce service de qualité ! Pour la voiture elle est top !', '2020-11-13 21:47:33');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `password`, `role`) VALUES
-(1, 'Mickael', 'mickael.jackson@gmail.com', '$2y$13$iPxpQ6ApKb9Ucs78tZGU1OWXfdRIsuC5w2nQfctYucoTtyL8sOKM.', 'ROLE_RENTER'),
-(2, 'UPS', 'ups24@gmail.com', '$2y$13$EZrbaezWe6i/OGQs8L9qOO5g/rNrOd2ttZyPYo2PrYZUbvHeJL9RW', 'ROLE_CLIENT'),
-(3, 'Daniel', 'daniel.aguiar@outlook.fr', '$2y$13$.YC3uvMRTp0FCdc3nKeesexhYSPkCRcHUybQZFC/Q03r9SiP6h3sy', 'ROLE_RENTER'),
-(4, 'David', 'benibri.david@gmail.com', '$2y$13$1RxweOK7IyZH8Rk32KOgJeA4zFkXMwQdlC9x7JPHAxrA6mzsjh.yW', 'ROLE_CLIENT'),
-(5, 'Benibri', 'benibri.david1@gmail.com', '$2y$13$VUlitBPDHMEX4LLPGALZkeQkrS3z03qwEPPWf0LiSvLhPYQQEN5aO', 'ROLE_CLIENT'),
-(6, 'Hugo', 'hugo.miko@gmail.com', '$2y$13$1a44YT92WkgfsLhHdgeLduLK5MxfEa7FJn06BkRH2Ch8kRb3y1aY6', 'ROLE_CLIENT'),
-(7, 'Hubert', 'hubert@gmail.com', '$2y$13$9hsK.Vuzs9zHLlck5PY9ZObeh5uYwuSuSWWTHV4IXiARakRdJDNiG', 'ROLE_CLIENT'),
-(8, 'Sixt', 'sixt@sixt.com', '$2y$13$C2oW1hNw1gjvCATAjlfit.w.YV86t4RmmZecWsG6CXoIwRT48fp/G', 'ROLE_RENTER'),
-(9, 'admin', 'admin@gmail.com', '$2y$13$R0NiIzzlcJStq8.c2gDRPeMsivgAQ7iP1XVdZIgLCERk8ZnQKZ/5q', 'ROLE_ADMIN');
 
 --
 -- Indexes for dumped tables
@@ -193,25 +180,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `billing`
 --
 ALTER TABLE `billing`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `car`
 --
 ALTER TABLE `car`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
